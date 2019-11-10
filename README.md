de73ffb36dca26a67421516b814d6f85c414cccc
BrieanneH <howard.brieanne@gmail.com>
2019-11-09 23:10:07 -0600

    styling css

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-generator
+
+For this activity I 
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-
+//create string elements
 var lowerCase="abcdefghijklmnopqrstuvwxyz";
 var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers="0123456789";
 var chars="!@#$%^&*()?~";
 
-
+//create user input value
 var selected= "";
 
 
@@ -14,6 +14,7 @@ var selected= "";
 function generate(){ 
     var numChar = prompt("How many charaters would you like to use?(8-128)");
 
+        //create loop when condition is false
         if (numChar < 8){
             alert("Not enough charaters. Try again.");
             return;
@@ -30,11 +31,12 @@ function generate(){
     
     var confirmUpper = confirm("Do you want upper case letters?");
      if(confirmUpper){ 
+         //picking a random charater from selected string
          selected = selected + upperCase;
         
 
 
-        //get random element from selected
+    
      
 
 
@@ -75,10 +77,14 @@ function generate(){
         password= selected[index]+password;
     } 
 
-
     alert(password)
+    document.getElementById("text").innerHTML = txt;
 
-    }
+}    
+  
+
+    
+        
 
    
    
